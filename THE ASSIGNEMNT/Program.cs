45c61d5768c4a8cdec044ff4ab663a31b47faa3a
339727042 <339727042@2260I-L29-020>
2024-09-06 08:39:15 -0400

    Add project files.

---
+++ THE ASSIGNEMNT/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace THE_ASSIGNEMNT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! My name is Joshua Figueiredo!");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used as information about code for other people to understand");
+            Console.ReadLine();
+            Console.WriteLine("JJJJJ  FFFFF");
+            Console.WriteLine("  J    F    ");
+            Console.WriteLine("  J    F    ");
+            Console.WriteLine("  J    FFF  ");
+            Console.WriteLine("J J    F    ");
+            Console.WriteLine("J J    F    ");
+            Console.WriteLine(" J     F    ");
+        }
+    }
+}
